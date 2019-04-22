7471598ab696eb1adee54115cc31cab179f06dda
Lucas Melo <luucasmell@gmail.com>
2019-04-22 09:01:39 -0300

    Adicionar anotacoes sobre this, settimeout, setinterval e queryselectorall

---
+++ script.js
@@ -0,0 +1,52 @@
+// let quadrado = document.querySelectorAll("div");
+
+// quadrado[3].style.backgroundColor="red"
+
+
+
+
+
+// SET INTERVAL E CLEAR INTERVAL
+
+// let botao = document.querySelector("button");
+// let ancora = document.querySelector("a");
+// let idIntervalo;
+
+// function abrirAlert2(){
+//   idIntervalo= setInterval(() => {
+//     alert("Olá")
+//   }, 2000);
+// }
+
+// function pararAlert(){
+//   clearInterval(idIntervalo);
+// }
+// botao.onclick = abrirAlert2;
+// ancora.onclick= pararAlert;
+
+
+
+// SET TIME OUT
+
+// function abrirAlert(){
+//   setTimeout(() => {
+//     alert("Olá");
+//     console.log("isso é um console.log");
+//   }, 1000);
+// }
+
+// botao.onclick= abrirAlert;
+
+
+
+// THIS
+
+// let quadrado = document.querySelector("div");
+// let quadrado2 = document.querySelector("aside");
+
+// function trocarCor(){
+//   this.innerHTML = "Olá gente"
+// }
+
+// quadrado.onclick= trocarCor;
+// quadrado2.onclick= trocarCor;
